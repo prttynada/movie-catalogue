343d95938dd4eb39f0368aea16877e4b6660ba1f
prttynada <prettinada@gmail.com>
2022-11-20 23:29:44 +0700

    Initialize movie catalogue automated testing implemented

---
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-view.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-multi-assign */
+/* eslint-disable operator-linebreak */
+/* eslint-disable comma-dangle */
+/* eslint-disable class-methods-use-this */
+
+import { createMovieItemTemplate } from '../../templates/template-creator';
+
+/* eslint-disable implicit-arrow-linebreak */
+class FavoriteMovieSearchView {
+  getTemplate() {
+    return `
+      <div class="content">
+        <input id="query" type="text">
+        <h2 class="content__heading">Your Liked Movie</h2>
+        <div id="movies" class="movies">
+        </div>
+      </div>
+    `;
+  }
+
+  runWhenUserIsSearching(callback) {
+    document.getElementById('query').addEventListener('change', (event) => {
+      callback(event.target.value);
+    });
+  }
+
+  showFavoriteMovies(movies = []) {
+    let html;
+    if (movies.length) {
+      html = movies.reduce(
+        (carry, movie) => carry.concat(createMovieItemTemplate(movie)),
+        ''
+      );
+    } else {
+      html = html = this._getEmptyMovieTemplate();
+    }
+    document.getElementById('movies').innerHTML = html;
+
+    document
+      .getElementById('movies')
+      .dispatchEvent(new Event('movies:updated'));
+  }
+
+  _getEmptyMovieTemplate() {
+    return '<div class="movie-item__not__found">Tidak ada film untuk ditampilkan</div>';
+  }
+}
+
+export default FavoriteMovieSearchView;
